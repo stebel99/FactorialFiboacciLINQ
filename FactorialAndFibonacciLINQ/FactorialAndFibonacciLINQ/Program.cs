e9ebbdc1683dac2a01fc1a086de62d0ba7444c4c
Danil Lebets <stebel99@gmail.com>
2020-03-07 17:57:02 +0200

    add factorial method

--- FactorialAndFibonacciLINQ/FactorialAndFibonacciLINQ/Program.cs
+++ FactorialAndFibonacciLINQ/FactorialAndFibonacciLINQ/Program.cs
@@ -10,6 +10,39 @@ namespace FactorialAndFibonacciLINQ
     {
         static void Main(string[] args)
         {
+            string input = "";
+            for (; ; )
+            {
+                Console.Clear();
+                Console.Write(@"Find Factorial
+Input number: ");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out int result))
+                {
+                    Console.WriteLine($@"{result}! = {Factorial(result)}");
+                    Console.Write("Press any key...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            int[] numbers = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            var result = numbers.Aggregate((x, y) => x * y);
+            return result;
         }
     }
 }
