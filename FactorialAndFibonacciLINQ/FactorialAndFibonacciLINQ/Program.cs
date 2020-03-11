b5937800df737c3e0ec9ee5fe4ee949a83952644
Danil Lebets <stebel99@gmail.com>
2020-03-11 15:47:14 +0200

    changed ui

--- FactorialAndFibonacciLINQ/FactorialAndFibonacciLINQ/Program.cs
+++ FactorialAndFibonacciLINQ/FactorialAndFibonacciLINQ/Program.cs
@@ -9,52 +9,61 @@ namespace FactorialAndFibonacciLINQ
         static void Main()
         {
             string userInput;
-            for (; ; )
+            bool run = true;
+            while (run)
             {
                 Console.Clear();
-                Console.Write(@"Find Factorial
-Input number: ");
+                Console.Write(@"1. Find Factorial
+2. Find Fibonacci
+3. Models with enum
+4. Arrays with numbers
+5. Exit
+Select action: ");
                 userInput = Console.ReadLine();
-                if (Int32.TryParse(userInput, out int result))
+                switch (userInput)
                 {
-                    Console.WriteLine($@"{result}! = {Factorial(result)}");
-                    Console.Write("Press any key...");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    continue;
-                }
-                Console.Clear();
-                Console.Write(@"Number Fibonacci
+                    case "1":
+                        {
+                            Console.Clear();
+                            Console.Write(@"Find Factorial
 Input number: ");
-                userInput = Console.ReadLine();
-                if (Int32.TryParse(userInput, out int result2))
-                {
-                    var numbers = Fibonacci(result2);
-                    foreach (var item in numbers)
-                    {
-                        Console.WriteLine(item);
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-
-                Console.Write("Again (Y)/(N):");
-                string again = Console.ReadLine();
-                if (again.ToUpper() == "Y")
-                {
-                    continue;
-                }
-                else 
-                {
-                    break;
+                            userInput = Console.ReadLine();
+                            if (Int32.TryParse(userInput, out int result))
+                            {
+                                Console.WriteLine($@"{result}! = {Factorial(result)}");
+                                Console.Write("Press any key...");
+                                Console.ReadKey();
+                            }
+                            continue;                           
+                        }
+                    case "2":
+                        {
+                            Console.Clear();
+                            Console.Write(@"Number Fibonacci
+Input number: ");
+                            userInput = Console.ReadLine();
+                            if (Int32.TryParse(userInput, out int result2))
+                            {
+                                var numbers = Fibonacci(result2);
+                                foreach (var item in numbers)
+                                {
+                                    Console.WriteLine(item);
+                                }
+                            }
+                            Console.Write("Press any key... ");
+                            Console.ReadLine();
+                             continue;                         
+                        }
+                    case "5":
+                        {
+                            run = false;
+                            break;
+                        }
+                    default:
+                        continue;
                 }
             }
         }
-
         public static int Factorial(int number)
         {
             if (number == 0)
